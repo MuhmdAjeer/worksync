8f2dfa467fbfcdb1287cc4e6e26f01c6f4cf9d8f
muhmdajeer <muhdajeer@gmail.com>
2024-04-15 17:00:49 +0530

    web

--- fds/lib/schema/Workspace.ts
+++
@@ -1,7 +0,0 @@
-import { ZodType, z } from "zod";
-import { OnboardingWorkspace } from "../types/Workspace";
-
-export const onboardingSchema: ZodType<OnboardingWorkspace> = z.object({
-  name: z.string(),
-  slug: z.string(),
-});
--- fds/lib/types/Workspace.ts
+++
@@ -1,4 +0,0 @@
-export interface OnboardingWorkspace {
-  name: string;
-  slug: string;
-}
